e36650734e1c9a4d7e084c3b218241ea2af97b68
Craig Marshall <craig9@gmail.com>
2019-03-20 13:37:30 +1100

    Added comments

--- Enigma.py
+++ Enigma.py
@@ -2,8 +2,6 @@
 
 import string
 
-debugging = True
-
 wheel_count = 3
 
 wheels = [("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "R"),
@@ -18,21 +16,26 @@ wheels = [("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "R"),
 reflectors = ["EJMZALYXVBWFCRQUONTSPIKHGD",
               "YRUHQSLDPXNGOKMIEBFZCWVJAT",
               "FVPJIAOYEDRZXWGCTKUQSBNMHL"]
+
 class Wheel:
-    def __init__(self, sequence, ticks):
-        self.input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    """A wheel class to hold the input and output strings,
+       with the ability to rotate its string an arbitrary
+       amount, and to encode and reverse encode a character"""
+
+    def __init__(self, sequence, notches):
+        self.input = string.ascii_uppercase
         self.output = sequence
-        self.ticks = ticks
+        self.notches = notches
 
     def rotate(self, amount = 1):
+        """Simulate a cylinder rotating"""
         self.input = self.input[amount:] + self.input[:amount]
         self.output = self.output[amount:] + self.output[:amount]
 
-    def __str__(self):
-        return self.input + " / " + self.output + " / " + self.ticks
-
-    def tick_activated(self):
-        return self.input[0] in self.ticks
+    def notch_active(self):
+        """If the first character of the input string is one of the
+           characters in the self.notches string, return True, else False"""
+        return self.input[0] in self.notches
 
     def encode(self, char, reverse=False):
         temp = self.input[string.ascii_uppercase.index(char)]
@@ -44,6 +47,10 @@ class Wheel:
         return temp                
 
 def get_wheel_choices():
+    """Gets a list of 0-based integers which is wheel_count items 
+       long from the user. Validates input, and re-asks if the user
+       gives a nonsense answer"""
+
     wheel_choices = []
 
     while len(wheel_choices) < wheel_count:
@@ -58,6 +65,10 @@ def get_wheel_choices():
     return wheel_choices
 
 def get_wheel_settings():
+    """Asks the user for a-z choices for wheel settings, returning a list of
+       wheel_count length, with 0-based integers denoting the a-z positions
+       chosen by the user"""
+
     wheel_settings = []
     
     while len(wheel_settings) < wheel_count:
@@ -73,6 +84,8 @@ def get_wheel_settings():
     return wheel_settings
 
 def get_reflector():
+    """Asks the user a, b, or c reflector, and returns 0, 1, or 2"""
+
     while True:
         choice = input("Please choose a reflector, a, b, or c: ")
         if choice in "abc":
@@ -81,6 +94,8 @@ def get_reflector():
             print("That was an invalid choice")
 
 def get_message():
+    """Get the users input message. Don't allow characters other than a-z"""
+
     while True:
         message = input("Please enter the message to encrypt: ").upper()
         for char in message:
@@ -90,17 +105,7 @@ def get_message():
         else:
             return message
 
-def main():
-    wheel_choices = get_wheel_choices()
-    wheel_settings = get_wheel_settings()
-    reflector_choice = get_reflector()
-    message = get_message()
-    print("Thank you")
-    print()
-
-    working_wheels = [Wheel(wheels[i][0], wheels[i][1]) for i in wheel_choices]
-    working_reflector = reflectors[reflector_choice]
-
+def encrypt(working_wheels, wheel_settings, working_reflector, message):
     for i in range(len(working_wheels)):
         w = working_wheels[i]
         s = wheel_settings[i]
@@ -109,27 +114,53 @@ def main():
     # Now for the fun bit
     encrypted = []
     for char in message:
+        #Rotate wheels if the notches are active
         working_wheels[2].rotate()
-        if working_wheels[2].tick_activated():
+        if working_wheels[2].notch_active():
             working_wheels[1].rotate()
-            if working_wheels[1].tick_activated():
+            if working_wheels[1].notch_active():
                 working_wheels[0].rotate()
 
         #FIXME Plugboard is missing entirely
 
+        #Encode with each wheel, in reverse order
         for i in range(len(working_wheels)-1, -1, -1):
             char = working_wheels[i].encode(char)
         
+        #Encode with the reflector
         char = working_reflector[string.ascii_uppercase.index(char)]
 
+        #"Reverse" Encode with each wheel in standard order
         for i in range(len(working_wheels)):
             char = working_wheels[i].encode(char, reverse=True)
 
         #FIXME Plugboard would happen here too
 
+        #Save the encrypted character
         encrypted.append(char)
 
-    print("Your encrypted text:", "".join(encrypted))
+    return "".join(encrypted)
+
+def main():
+    """Gather the users wheel choices, settings, reflector choice and message,
+       encrypt the message, and print the result to the screen"""
+    wheel_choices = get_wheel_choices()
+    wheel_settings = get_wheel_settings()
+    reflector_choice = get_reflector()
+    message = get_message()
+
+    # wheel_choices is a list of ints, turn it into a list of tuples. Each
+    # tuple has the connections (relative to a standard alphabet) in string format,
+    # and the notch positions as a second string in the tuple
+    working_wheels = [Wheel(wheels[i][0], wheels[i][1]) for i in wheel_choices]
+
+    # Take the integer reflector_choice and make us a working copy of that reflector
+    working_reflector = reflectors[reflector_choice]
+
+    # The meat and potatoes
+    encrypted = encrypt(working_wheels, wheel_settings, working_reflector, message)
+
+    print("Your encrypted text:", encrypted)
 
 if __name__ == "__main__":
     main()
